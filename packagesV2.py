dbf7f1cd009d438484ce97054b084c6cf24a8e28
AbangRombeng <80770646+SUBUR78990@users.noreply.github.com>
2023-07-13 11:40:52 +0700

    Add files via upload

---
+++ packagesV2.py
@@ -0,0 +1,119 @@
+# Decompile by Mardis (Tools By Kapten-Kaizo)
+# Time Succes decompile : 2023-06-23 19:31:00.990044
+import re, bs4, requests, rich, platform, time, sys, os, random, json, time
+from rich.panel import Panel as UsydPanel
+from rich.console import Console as UsydConsole
+from concurrent.futures import ThreadPoolExecutor as TheEND
+import os
+import time
+import sys
+dumps,payload=[],{}
+os.system("clear")
+
+print ('''\033[36m		       __
+  .-----..---.-..----.|  |--..---.-..-----..-----..-----.
+  |  _  ||  _  ||  __||    < |  _  ||  _  ||  -__||__ --|
+  |   __||___._||____||__|__||___._||___  ||_____||_____|
+  |__|                              |_____|              
+  ''')
+def slowprint(s):
+    for c in s + '\n' :
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(5. / 300)
+
+print (''' \033[92m  +--------------------------------------------------+
+   | List packages di bawah ini akan menginstall auto |
+   +--------------------------------------------------+
+                    \033[91m Tools Gratis''')
+slowprint(''' \033[93m
+\033[36mPEMBUAT : SUBUR.M
+-----------------
+ 
+\033[92m[\033[91m00\033[92m] \033[93mfish		\033[92m[\033[91m02\033[92m] \033[93mopenssl		\033[92m[\033[91m03\033[92m] \033[93mtoilet
+\033[92m[\033[91m04\033[92m] \033[93mpython		\033[92m[\033[91m05\033[92m] \033[93mopenssh		\033[92m[\033[91m06\033[92m] \033[93mfiglet
+\033[92m[\033[91m07\033[92m] \033[93mpython2		\033[92m[\033[91m08\033[92m] \033[93mwget		\033[92m[\033[91m09\033[92m] \033[93mcoreutils
+\033[92m[\033[91m10\033[92m] \033[93mpython-dev		\033[92m[\033[91m11\033[92m] \033[93mclang		\033[92m[\033[91m12\033[92m] \033[93mgc
+\033[92m[\033[91m13\033[92m] \033[93mpython3		\033[92m[\033[91m14\033[92m] \033[93mnmap		\033[92m[\033[91m15\033[92m] \033[93mpip
+\033[92m[\033[91m16\033[92m] \033[93mphp		\033[92m[\033[91m17\033[92m] \033[93mw3m		\033[92m[\033[91m18\033[92m] \033[93msqlmap
+\033[92m[\033[91m19\033[92m] \033[93mjava		\033[92m[\033[91m20\033[92m] \033[93mhydra		\033[92m[\033[91m21\033[92m] \033[93mcmatrix
+\033[92m[\033[91m22\033[92m] \033[93mgit		\033[92m[\033[91m23\033[92m] \033[93mruby		\033[92m[\033[91m24\033[92m] \033[93mtor
+\033[92m[\033[91m25\033[92m] \033[93mperl		\033[92m[\033[91m26\033[92m] \033[93mmacchanger		\033[92m[\033[91m27\033[92m] \033[93mtree
+\033[92m[\033[91m28\033[92m] \033[93mbash		\033[92m[\033[91m29\033[92m] \033[93mhost		\033[92m[\033[91m30\033[92m] \033[93mhollywood
+\033[92m[\033[91m31\033[92m] \033[93mnano		\033[92m[\033[91m32\033[92m] \033[93mdnsutils		\033[92m[\033[91m33\033[92m] \033[93mtop
+\033[92m[\033[91m34\033[92m] \033[93mcurl		\033[92m[\033[91m35\033[92m] \033[93mlolcat		\033[92m[\033[91m36\033[92m] \033[93mtermux-api
+''')
+slowprint('''\033[96m
+TEKAN\033[93m (\033[31mY\033[93m)\033[96m untuk menginstall
+TEKAN\033[93m (\033[31mN\033[93m)\033[96m untuk keluar program
+------------------------------
+\033[93m[\033[92mSemoga Program ini Bermanfaat follow github Admin & START\033[93m]''')
+
+print ("                                            ")
+choice = input("\033[93mLanjut & Enter [y/n] : ")
+os.system('xdg-open https://github.com/SUBUR78990?tab=followers')
+if choice == 'n' : sys.exit()
+if choice == 'y' : os.system ("apt-get update -y")
+os.system ("apt-get upgrade -y")
+os.system ("apt install python -y")
+os.system ("apt install python2 -y")
+os.system ("apt install php -y")
+os.system ("apt install python-dev -y")
+os.system ("apt install python3 -y")
+os.system ("apt install java -y")
+os.system ("apt install git -y")
+os.system ("apt install perl -y")
+os.system ("apt install bash")
+os.system ("apt install fish")
+
+
+print ("wait for second and start hacking")
+
+os.system ("apt install nano -y")
+os.system ("apt install curl -y")
+os.system ("apt install openssl -y")
+os.system ("apt install openssh -y")
+os.system ("apt install wget -y")
+os.system ("apt install clang -y")
+os.system ("apt install nmap -y")
+os.system ("apt install w3m -y")
+os.system ("apt install hydra -y")
+os.system ("apt install fish -y")
+os.system ("pkg install cmatrix -y")
+os.system ("pkg install tor -y")
+os.system ("pkg install tree -y")
+os.system ("pkg install top -y")
+print ("""
+subscribe VPP Hacker """)
+
+os.system ("apt install ruby -y")
+os.system ("apt install macchanger -y")
+os.system ("apt install host -y")
+os.system ("apt install dnsutils -y")
+os.system ("apt install coreutils -y")
+os.system ("gem install lolcat")
+os.system ("apt install toilet -y")
+os.system ("apt install figlet -y")
+os.system ("apt install gc -y")
+os.system ("apt install pip -y")
+os.system ("pip install sqlmap")
+os.system ("pkg install hollywood -y")
+print ("Tekan Izinkan untuk membaca Storage")
+
+os.system("clear")
+os.system ("termux-setup-storage")
+
+def slowprint(s):
+    for c in s + '\n' :
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(8. / 100)
+
+slowprint('''\033[96m+------------------------------------+
+|       Thanks for watching          |
+|  Subscribe : http://bit.ly/2ZUcZvP |
+|     YouTube : Abash-T Channel      |
++------------------------------------+
+''')
+input("\n\npenginstallan selesai. ENTER : ")
+
